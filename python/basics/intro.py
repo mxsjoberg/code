6c9d239d1edc07aff287fc801fb4255c3a0317eb
Michael Sjöberg <random@michaelsjoberg.com>
2020-10-10 22:25:36 +0200

    Update intro.py

--- python/basics/intro.py
+++ python/basics/intro.py
@@ -1,19 +1,11 @@
-# filename.py
-
 #!/usr/bin/python
 
 def main():
     # do something
-    
+
     return
 
 if (__name__ == "__main__"):
-    main()
+	main()
 
-# build and run: $ python filename.py
-
-'''
-Michael Sjoeberg
-2018-11-05
-https://github.com/michaelsjoeberg/python-playground/blob/master/basics/intro.py
-'''
+# build and run: $ python <filename>.py
