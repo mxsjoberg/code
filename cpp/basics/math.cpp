530bc4a6b28e3b77e8cad207e768a81913904a4c
Michael Sjöberg <random@michaelsjoberg.com>
2020-10-18 20:35:05 +0200

    Create math.cpp

---
+++ cpp/basics/math.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+
+// constants
+M_PI 							// 3.14159
+M_E								// 2.71828
+
+// numerical operations
+floor(2.945) 					// 2
+trunc(2.945) 					// 2
+exp(1) 							// 2.71828
+sqrt(16) 						// 4
+
+// NOTE: no built-in for factorial
+
+// EXAMPLE
+sin(4 * M_PI / 180) 			// 0.0697565
+
+// find more at http://www.cplusplus.com/reference/cmath
