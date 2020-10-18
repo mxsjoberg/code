caacb3e288773b4758db6cb619b0786c776f23be
Michael Sjöberg <random@michaelsjoberg.com>
2020-10-18 20:35:02 +0200

    Create arithmetics.c

---
+++ cpp/basics/arithmetics.c
@@ -0,0 +1,52 @@
+10 + 20                         // 30
+20 - 10                         // 10
+10 * 20                         // 200
+20 / 10                         // 2
+
+// floating point conversion
+10.0 + (10 + 20)                // 40
+
+// NOTE: char is promoted to int value
+'c' + 10
+// 109 (ascii value of c is 99)
+
+// floating point conversion using type casting
+(double) 10.0 + (10 + 20)       // 40
+(double) 20.0 - (10 + 10)       // 0
+(double) 10.0 * (10 * 2)        // 200
+
+// common division error
+30 / 20                         // 1
+(double) 30 / 20                // 1.5
+
+// integer divison, quotient
+20 / 20                         // 1
+30 / 20                         // 1
+40 / 20                         // 2
+
+// modulo, remainder
+10 % 20                         // 10
+
+// increment and decrement
+int a = 10;
+
+a++                             // 11
+a--                             // 9
+
+// built-ins requires cmath
+#include <cmath>
+
+// power
+pow(10, 2)                      // 100
+
+// module, remainder on floating point
+fmod(12.5, 10.0)                // 2.5
+
+// built-ins
+abs(-20)                        // 20
+
+// rounding value
+round(2.945)                    // 3
+round(2.495)                    // 2
+
+// NOTE: no built-ins for round to decimal places
