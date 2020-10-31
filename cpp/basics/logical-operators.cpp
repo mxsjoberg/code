777915e2f678ea8cd3129c0f9a258e210b06e594
Michael Sjöberg <random@michaelsjoberg.com>
2020-10-31 16:32:30 +0100

    Update logical-operators.cpp

--- cpp/basics/logical-operators.cpp
+++ cpp/basics/logical-operators.cpp
@@ -1,4 +1,4 @@
-// define and initialise boolean variables
+// boolean variables
 bool T = true;
 bool F = false;
 
@@ -10,10 +10,21 @@ T && (T && F)           // 0
 !T                      // 0
 !(!T)                   // 1
 
-// equality
+// relational
 1 == 2                  // 0
 1 != 2                  // 1
 1 > 2                   // 0
 1 < 2                   // 1
 1 >= 2                  // 0
 1 <= 2                  // 1
+
+// bitwise operators
+int A = 60; 			// 0011 1100
+int B = 13; 			// 0000 1101
+
+A & B 					// 12, 0000 1100
+A | B 					// 61, 0011 1101
+A ^ B 					// 49, 0011 0001
+~A 						// -61, 1100 0011
+A << 2 					// 240, 1111 0000
+A >> 2 					// 15, 0000 1111
