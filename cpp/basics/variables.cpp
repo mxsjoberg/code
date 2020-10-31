318d8ce054298b0d456026d6a98242d865635e03
Michael Sjöberg <random@michaelsjoberg.com>
2020-10-31 16:32:32 +0100

    Update variables.cpp

--- cpp/basics/variables.cpp
+++ cpp/basics/variables.cpp
@@ -1,21 +1,21 @@
-// OUTSIDE SCOPE: global or header file
+// OUTSIDE SCOPE: global
 // variable declaration
 extern int x;
-extern char y[6]; 		// 
-extern float z; 		// 4 bytes
-extern double a; 		// 8 bytes
+extern char y[6];
+extern float z;             // 4 bytes
+extern double a;            // 8 bytes
 
 // INSIDE SCOPE: local
 // variable definition
 int x;
 
-// variable definition and initialisation
-char y[] = "String";        // String
-float z = 1.05;             // 1.05
-double a = 12.5;            // 12.5
-
 // variable initialisation
 x = 6;
+
+// variable definition and initialisation
+char y[] = "String";
+float z = 1.05;
+double a = 12.5;
 
 // multiple variable definition
 int c, d;
@@ -23,9 +23,17 @@ int c, d;
 // multiple variable initialisation
 c = d = 6;                  // 6, 6
 
+// multiple variable definition and initialisation
+int c = 6, d = 6;
+
 // type modifiers
-unsigned int unint; 		// 4 bytes, 0 to 4294967295
-signed int siint; 			// 4 bytes, -2147483648 to 2147483647
-short int sint; 			// 2 bytes, -32768 to 32767
-long int lint; 				// 8 bytes, -2147483648 to 2147483647
-long long int llint; 		// 8 bytes, -(2^63) to (2^63)-1
+unsigned int unint;         // 4 bytes, 0 to 4294967295
+signed int siint;           // 4 bytes, -2147483648 to 2147483647
+short int sint;             // 2 bytes, -32768 to 32767
+long int lint;              // 8 bytes, -2147483648 to 2147483647
+long long int llint;        // 8 bytes, -(2^63) to (2^63)-1
+
+// storage classes
+register int rint;          // make it possible to store local variable in register (instead of RAM)
+static int stint;           // keep local variable in existence during execution
+extern int eint;            // global variable visible to all program files
