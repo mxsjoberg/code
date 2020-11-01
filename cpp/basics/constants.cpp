817c97b7e70eee7962cd89f45863367d7bbce900
Michael Sjöberg <random@michaelsjoberg.com>
2020-11-01 11:34:36 +0100

    Update constants.cpp

--- cpp/basics/constants.cpp
+++ cpp/basics/constants.cpp
@@ -2,6 +2,6 @@
 #define LENGTH 10
 #define WIDTH 5
 
-// INSIDE SCOPE: use constants as variables
+// INSIDE SCOPE: use constants as substitute for assigned value
 LENGTH          // 10
 WIDTH           // 5
