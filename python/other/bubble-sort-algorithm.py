018110715223c712eb005565b1f2e6a4df602e48
Michael Sjöberg <random@michaelsjoberg.com>
2020-11-01 23:17:38 +0100

    Update bubble-sort-algorithm.py

--- python/other/bubble-sort-algorithm.py
+++ python/other/bubble-sort-algorithm.py
@@ -4,12 +4,12 @@ print(unsorted_list)
 # [14, 33, 27, 35, 10]
 
 for i in range(len(unsorted_list)):
-	for j in range(len(unsorted_list) - 1):
-		if unsorted_list[j] > unsorted_list[j + 1]:
-			temp = unsorted_list[j]
-			# swap positions
-			unsorted_list[j] = unsorted_list[j + 1]
-			unsorted_list[j + 1] = temp
+    for j in range(len(unsorted_list) - 1):
+        if unsorted_list[j] > unsorted_list[j + 1]:
+            temp = unsorted_list[j]
+            # swap positions
+            unsorted_list[j] = unsorted_list[j + 1]
+            unsorted_list[j + 1] = temp
 
 print(unsorted_list)
 # [10, 14, 27, 33, 35]
