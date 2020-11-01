082a1dc6826a246c42813f5202840e656eef7331
Michael Sjöberg <random@michaelsjoberg.com>
2020-11-01 20:11:00 +0100

    Create switch.cpp

---
+++ cpp/basics/switch.cpp
@@ -0,0 +1,15 @@
+int a = 1;
+
+switch (a) {
+    case 0:
+        // do something
+        break;
+    case 1:
+        // do something
+        break;
+    case 2:
+        // do something
+        break;
+    default:
+        // do something
+}
