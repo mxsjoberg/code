63ae05ad962f9c99f64997216255e00f67889b47
Michael Sjöberg <random@michaelsjoberg.com>
2020-11-01 19:54:56 +0100

    Create read-and-write.cpp

---
+++ cpp/basics/read-and-write.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+string input;
+
+// read input
+cin >> input;
+// input = 1 2 3 4
+
+// print input
+cout << input << endl;
+// 1
+
+// or getline to get line
+getline(cin, input);
+cout << input << endl;
+// 1 2 3 4
