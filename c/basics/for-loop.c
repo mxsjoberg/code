0c128a6212d35d699a20a40af4361ee160fa431b
Michael Sjöberg <random@michaelsjoberg.com>
2020-11-01 20:29:47 +0100

    Update for-loop.c

--- c/basics/for-loop.c
+++ c/basics/for-loop.c
@@ -1,12 +1,4 @@
-for (int a = 1; a < 5; a++) {
-    // do something
-}
-
-// iterate array
-#define ARRAY_SIZE 5
-int array[ARRAY_SIZE] = { 1, 2, 3, 4, 5 };
-
-for (int n = 0; n < ARRAY_SIZE; n++) {
+for (int i = 1; i < 5; i++) {
     // do something
 }
 
@@ -15,4 +7,12 @@ for (int i = 0; i < 2; i++) {
     for (int j = 0; j < 2; j++) {
         // do something
     }
+}
+
+// iterate array
+#define ARRAY_SIZE 5
+int array[ARRAY_SIZE] = { 1, 2, 3, 4, 5 };
+
+for (int i = 0; i < ARRAY_SIZE; i++) {
+    // do something
 }
