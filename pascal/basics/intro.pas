0b7009badcd7b58406f679657a319106076005e2
Michael Sjöberg <random@michaelsjoberg.com>
2021-04-28 12:10:33 +0200

    Update intro.pas

--- pascal/basics/intro.pas
+++ pascal/basics/intro.pas
@@ -1,13 +1,16 @@
 (*
-	Pascal is a procedural, strongly typed programming language developed by Niklaus Wirth in 1970.
+    Pascal is a procedural, strongly typed programming language developed by Niklaus Wirth in 1970.
 
-	https://en.wikipedia.org/wiki/Pascal_(programming_language)
+    https://en.wikipedia.org/wiki/Pascal_(programming_language)
 *)
 
 PROGRAM intro;
 BEGIN
-	(* do something *)
-	WRITELN('hello pascal');
+    // do something
+    WRITELN('hello pascal');
 END.
 
-(* run from command-line: fpc <filename>.pas *)
+{
+    run from command-line:
+        fpc <filename>.pas 
+}
