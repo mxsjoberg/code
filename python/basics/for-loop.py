a73ede2c80df4bef3edb48e391605051946e4971
Michael Sjöberg <random@michaelsjoberg.com>
2021-04-30 19:47:42 +0200

    Update for-loop.py

--- python/basics/for-loop.py
+++ python/basics/for-loop.py
@@ -2,20 +2,25 @@ numbers = [1, 2, 3, 4]
 
 for number in numbers:
     # do something
+    pass
 
 # nested
 for i in range(10):
     for j in range(10):
         # do something
+        pass
 
 # iterating dictionaries
-dict = {'Alpha': 1, 'Beta': 2}
+dict = { 'Alpha': 1, 'Beta': 2 }
 
-for key in dict.keys():
-    print (key)                     # Alpha Beta
+for key in dict.keys(): print(key)
+# Alpha
+# Beta
 
-for value in dict.values():
-    print (value)                   # 1 2
+for value in dict.values(): print(value)
+# 1
+# 2
 
-for key, value in dict.items():
-    print (key, value)              # ('Alpha', 1) ('Beta', 2)
+for key, value in dict.items(): print(key, value)
+# Alpha 1
+# Beta 2
