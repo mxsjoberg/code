30595bd401c1d75abb62f7dde2bad98687283a62
Michael Sjöberg <random@michaelsjoberg.com>
2021-04-30 19:36:35 +0200

    Update dictionaries.py

--- python/basics/dictionaries.py
+++ python/basics/dictionaries.py
@@ -1,26 +1,30 @@
 # dictionaries are hash tables, key:value pairs
-dict = {'Adam': ['adam@email.com', 2445055],
-        'Bard': 'bard@email.com'}
+dct = {
+    'Adam': ['adam@email.com', 2445055],
+    'Bard': 'bard@email.com'
+}
 
-dict                    # {'Adam': ['adam@email.com', 2445055], 'Bard': 'bard@email.com'}
-dict['Adam']            # ['adam@email.com', 2445055]
-dict['Adam'][1]         # 2445055
+print(dct)                              # {'Adam': ['adam@email.com', 2445055], 'Bard': 'bard@email.com'}
+print(dct['Adam'])                      # ['adam@email.com', 2445055]
+print(dct['Adam'][1])                   # 2445055
 
 # update value
-dict['Bard'] = 'bard@anotheremail.com'
-dict                    # {'Adam': ['adam@email.com', 2445055], 'Bard': 'bard@anotheremail.com'}
+dct['Bard'] = 'bard@anotheremail.com'
+print(dct)
+# {'Adam': ['adam@email.com', 2445055], 'Bard': 'bard@anotheremail.com'}
 
 # add key:value pair
-dict['Cole'] = 'cole@email.com'
-dict                    # {'Cole': 'cole@email.com', 'Adam': ['adam@email.com', 2445055], 'Bard': 'bard@anotheremail.com'}
+dct['Cole'] = 'cole@email.com'
+print(dct)
+# {'Adam': ['adam@email.com', 2445055], 'Bard': 'bard@anotheremail.com', 'Cole': 'cole@email.com'}
 
 # remove key:value pair
-del dict['Cole']        # {'Adam': ['adam@email.com', 2445055], 'Bard': 'bard@anotheremail.com'}
+del dct['Cole']
 
-'Cole' in dict          # False
-'Adam' in dict          # True
+print('Cole' in dct)                    # False
+print('Adam' in dct)                    # True
 
 # create dictionary from a list of tuples
-dict_list_tuples = dict([(1, "x"), (2, "y"), (3, "z")])
-dict_list_tuples
+dct_list_tuples = dict([(1, "x"), (2, "y"), (3, "z")])
+print(dct_list_tuples)
 # {1: 'x', 2: 'y', 3: 'z'}
