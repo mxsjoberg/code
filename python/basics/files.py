d99b718a371e8dc67acf11910eafeccb739e837f
Michael Sjöberg <random@michaelsjoberg.com>
2021-04-30 19:42:39 +0200

    Update files.py

--- python/basics/files.py
+++ python/basics/files.py
@@ -1,15 +1,20 @@
+# create file
+with open('test.txt', 'w') as file:
+    pass
+
 # read from file
-with open('path/to/file', 'r') as file:
+with open('test.txt', 'r') as file:
     file.read()
 
 # or
-content = open('path/to/file', 'r').read()
+content = open('test.txt', 'r').read()
 
 # read lines
-with open('path/to/file.txt', 'r') as file:
+with open('test.txt', 'r') as file:
     for line in file.readlines():
+        pass
         # do something
 
 # write to file
-with open('path/to/file.txt', 'w') as file:
+with open('test.txt', 'w') as file:
     file.write('This is some text to write.')
