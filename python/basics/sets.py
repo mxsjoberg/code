e90be59c0afbb9c8802e82903b34ec03562bbab9
Michael Sjöberg <random@michaelsjoberg.com>
2021-04-30 20:24:02 +0200

    Update sets.py

--- python/basics/sets.py
+++ python/basics/sets.py
@@ -1,30 +1,35 @@
 # sets are unordered collections
 set = {1.0, 10, 'String', (1, 0, 1, 0)}
-set                     # set([(1, 0, 1, 0), 10, 'String', 1.0])
+print(set)
+# {(1, 0, 1, 0), 1.0, 10, 'String'}
 
-'String' in set         # True
-'Java' in set           # False
+print('String' in set)                  # True
+print('Java' in set)                    # False
 
 # add to set
-set.add('Python')       # set(['Python', (1, 0, 1, 0), 10, 'String', 1.0])
+set.add('Python')
+print(set)
+# {1.0, 'String', (1, 0, 1, 0), 10, 'Python'}
 
 # remove from set
-set.remove('Python')    # set([(1, 0, 1, 0), 10, 'String', 1.0])
+set.remove('Python')
+print(set)
+# {1.0, 'String', (1, 0, 1, 0), 10}
 
 # set operations
-set_one = {1, 2, 3}     # set([1, 2, 3])
-set_two = {3, 4, 5}     # set([3, 4, 5])
+set_one = {1, 2, 3}
+set_two = {3, 4, 5}
 
-set_one | set_two       # set([1, 2, 3, 4, 5])      (Union)
-set_one & set_two       # set([3])                  (Intersection)
-set_one - set_two       # set([1, 2])               (Difference)
-set_one ^ set_two       # set([1, 2, 4, 5])         (Symmetric difference)
+print(set_one | set_two)                # {1, 2, 3, 4, 5}   (Union)
+print(set_one & set_two)                # {3}               (Intersection)
+print(set_one - set_two)                # {1, 2}            (Difference)
+print(set_one ^ set_two)                # {1, 2, 4, 5}      (Symmetric difference)
 
 # subset and superset
 set_a = {1, 2}
 set_b = {1, 2}
 set_c = {1, 2, 3, 4, 5}
 
-set_a < set_b           # False                     (Strict subset)
-set_a <= set_b          # True                      (Subset)
-set_c > set_a           # True                      (Strict superset)
+print(set_a < set_b)                    # False             (Strict subset)
+print(set_a <= set_b)                   # True              (Subset)
+print(set_c > set_a)                    # True              (Strict superset)
