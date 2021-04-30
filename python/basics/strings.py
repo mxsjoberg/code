0a8d35d7941c3e0efc25d7573b39309518c68ab3
Michael Sjöberg <random@michaelsjoberg.com>
2021-04-30 20:24:05 +0200

    Update strings.py

--- python/basics/strings.py
+++ python/basics/strings.py
@@ -1,33 +1,37 @@
 string = 'proton neutron'
-string[:]                               # proton neutron
-string[:2]                              # pr
-string[-2:]                             # on
-string[1:3]                             # ro
+
+print(string[:])                            # proton neutron
+print(string[:2])                           # pr
+print(string[-2:])                          # on
+print(string[1:3])                          # ro
 
 # reverse a string
-string[::-1]                            # notorp
+print(string[::-1])
+# notorp
 
 # skip every second character
-string[0:-1:2]                          # poo
+print(string[0:-1:2])
+# poo
 
 # built-ins
-string.capitalize()                     # Proton neutron
-string.title()                          # Proton Neutron
-string.upper()                          # PROTON NEUTRON
-string.lower()                          # proton neutron
-string.center(20, '.')                  # ...proton neutron...
+print(string.capitalize())                  # Proton neutron
+print(string.title())                       # Proton Neutron
+print(string.upper())                       # PROTON NEUTRON
+print(string.lower())                       # proton neutron
+print(string.center(20, '.'))               # ...proton neutron...
 
 # conditionals
-string.isdigit()                        # False
-string.islower()                        # True
-string.isupper()                        # False
+print(string.isdigit())                     # False
+print(string.islower())                     # True
+print(string.isupper())                     # False
 
 # counting and finding letters in text
-string.count('p', 0, len(string))       # 1
-string.find('t', 1, len(string))        # 3
+print(string.count('p', 0, len(string)))    # 1
+print(string.find('t', 1, len(string)))     # 3
 
-# removing whitespace from a string
+# removing whitespace from string
 string = '    some text    '
-string.lstrip()                         # 'some text    '
-string.rstrip()                         # '    some text'
-string.strip()                          # 'some text'
+
+print(string.lstrip())                      # 'some text    '
+print(string.rstrip())                      # '    some text'
+print(string.strip())                       # 'some text'
