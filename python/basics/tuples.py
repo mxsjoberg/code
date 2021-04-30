c166f5292226e6aa50317fe8e690a44df8517d2d
Michael Sjöberg <random@michaelsjoberg.com>
2021-05-01 00:08:59 +0200

    Update tuples.py

--- python/basics/tuples.py
+++ python/basics/tuples.py
@@ -2,14 +2,15 @@
 tuple_one = (1.0, 'String', 4)
 tuple_two = ('Alpha', 'Bravo', (1, 0))
 
-tuple_one                           # (1.0, 'String', 4)
-tuple_two                           # ('Alpha', 'Bravo', (1, 0))
-tuple_two[2][1]                     # 0
+print(tuple_one)                            # (1.0, 'String', 4)
+print(tuple_two)                            # ('Alpha', 'Bravo', (1, 0))
+print(tuple_two[2][1])                      # 0
 
 # concatenate tuples
-tuple = tuple_one + tuple_two
+tuples = tuple_one + tuple_two
+print(tuples)
 # (1.0, 'String', 4, 'Alpha', 'Bravo', (1, 0))
 
 # create tuple from a list
-tuple_list = tuple([100, 'B'])
+print(tuple([100, 'B']))
 # (100, 'B')
