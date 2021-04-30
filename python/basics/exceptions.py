3428c5cc8bca2b8db253c1c48e61eb8f3d02e9ab
Michael Sjöberg <random@michaelsjoberg.com>
2021-04-30 19:42:36 +0200

    Update exceptions.py

--- python/basics/exceptions.py
+++ python/basics/exceptions.py
@@ -1,13 +1,15 @@
 try:
+    pass
     # do something
 
 except ZeroDivisionError:
-    print ('Cannot divide with zero.')
+    print('Cannot divide by zero.')
     # do something
 
 except Exception as e:
-    print ('Error: ' + str(e))
+    print('Error: ' + str(e))
     # do something
 
 finally:
+    pass
     # do something
