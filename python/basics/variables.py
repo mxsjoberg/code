218ee8637bda2a99329e508919f2f0e16b0f0950
Michael Sjöberg <random@michaelsjoberg.com>
2021-05-01 00:09:00 +0200

    Update variables.py

--- python/basics/variables.py
+++ python/basics/variables.py
@@ -4,32 +4,38 @@ y = 'String'
 z = 1.05
 a = x
 
-x, y, z, a                  # (6, 'String', 1.05, 6)
+print(x, y, z, a)
+# 6 String 1.05 6
 
 # types
-type(x)                     # int
-type(y)                     # str
-type(z)                     # float
-type(a)                     # int
+print(type(x))              # <class 'int'>
+print(type(y))              # <class 'str'>
+print(type(z))              # <class 'float'>
+print(type(a))              # <class 'int'>
 
 # multiple assignment
 x = a = 6
 y, z = 'String', 1.05
 
-x, y, z, a                  # (6, 'String', 1.05, 6)
+print(x, y, z, a)
+# 6 String 1.05 6
 
 # list assignment
-d, t, v = [230, 45, 12]     # (230, 45, 12)
+d, t, v = [230, 45, 12]
+print(d, t, v)
+# 230 45 12
 
 # string assignment
-a, b, c, d = '100B'         # ('1', '0', '0', 'B')
+a, b, c, d = '100B'
+print(a, b, c, d)
+# 1 0 0 B
 
 # or
 string = '100B'
-number = string[:3]
-letter = string[3:]
-number, letter              # ('100', 'B')
+print(string[:3])           # 100
+print(string[3:])           # B
 
 # delete a variable
-del n
-n                           # NameError: name 'n' is not defined
+del x
+print(x)
+# NameError: name 'x' is not defined
