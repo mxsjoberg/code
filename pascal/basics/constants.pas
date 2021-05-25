bed1dddb797d183f5b3461669aafc8fe7ad617bc
Michael Sjöberg <random@michaelsjoberg.com>
2021-05-26 00:51:41 +0200

    Update constants.pas

--- pascal/basics/constants.pas
+++ pascal/basics/constants.pas
@@ -3,8 +3,10 @@ CONST
     LENGTH = 10;
     HEIGHT = 15;
 BEGIN
-    WRITELN(LENGTH * HEIGHT);           // 150
+    WRITELN(LENGTH * HEIGHT);
+    // 150
 
     // constants are immutable
-    LENGTH := 5;                        // Error: Variable identifier expected
+    LENGTH := 5;
+    // Error: Variable identifier expected
 END.
