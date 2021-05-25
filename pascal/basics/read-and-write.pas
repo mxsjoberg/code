901a3f5a5be07ee0453c726c96ecb08d00a10291
Michael Sjöberg <random@michaelsjoberg.com>
2021-05-25 22:05:58 +0200

    Create read-and-write.pas

---
+++ pascal/basics/read-and-write.pas
@@ -0,0 +1,8 @@
+PROGRAM read_and_write;
+VAR
+    input: string;
+BEGIN
+    WRITELN('Write something:');    // Write something:
+    READLN(input);                  // > hello pascal
+    WRITELN(input);                 // hello pascal
+END.
