6c265d4ddfe8fa6e0353aea2ac91cd3ba6b6c020
Michael Sjöberg <random@michaelsjoberg.com>
2021-05-26 01:09:00 +0200

    Update arithmetic-operators.pas

--- pascal/basics/arithmetic-operators.pas
+++ pascal/basics/arithmetic-operators.pas
@@ -1,6 +1,6 @@
 PROGRAM arithmetic_operators;
 BEGIN
-    WRITELN(10 + 20);               // 30
+    WriteLn(10 + 20);               // 30
     WRITELN(20 - 10);               // 10
     WRITELN(10 * 20);               // 200
     WRITELN(20 / 10);               // 2.0...
