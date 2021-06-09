24cc44351805445c30552eaf1d5d7331091ac771
Michael Sjöberg <random@michaelsjoberg.com>
2021-06-09 23:40:35 +0200

    Update exceptions.cpp

--- cpp/basics/exceptions.cpp
+++ cpp/basics/exceptions.cpp
@@ -1,24 +1,29 @@
-try {
-    // do something
+#include <iostream>
+using namespace std;
 
-    throw 0;
-} catch (int error) {
-    // do something
-    
-    cout << "Error: " << error << endl;
-    // Error: 0
-}
-
-// EXAMPLE: default exception
-bool error = true;
-try {
-    if (!error) {
+int main() {
+    // try-catch
+    try {
         // do something
-    } else {
-        // error code
+
         throw 0;
+    } catch (int error) {
+        // do something
+        
+        cout << "Error: " << error << endl;
+        // Error: 0
     }
-} catch (...) {
-    cout << "Default exception." << endl;
-    // Default exception.
+    // default exception
+    try {
+        if (!true) {
+            // do something
+        } else {
+            // error code
+            throw 0;
+        }
+    } catch (...) {
+        cout << "Default exception." << endl;
+        // Default exception.
+    }
+    return 0;
 }
