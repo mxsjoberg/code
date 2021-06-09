765db1b96435134c3a57c945ca81dbf9f5a963fe
Michael Sjöberg <random@michaelsjoberg.com>
2021-06-09 13:32:11 +0200

    Create intro.v

---
+++ verilog/basics/intro.v
@@ -0,0 +1,31 @@
+/*
+    Verilog is a structured, hardware description language developed by Prabhu Goel et al in 1984.
+
+    https://en.wikipedia.org/wiki/Verilog
+*/
+
+// module < filename > (< port list >)
+module intro;
+    initial 
+    begin
+        $display("hello verilog");
+        $finish;
+    end
+endmodule
+
+/*
+    online simulator:
+        http://www.techep.csi.cuny.edu/~zhangs/v.html
+    
+    run from command-line:
+        $ iverilog -o intro intro.v
+        $ vvp intro
+
+    build system (sublime text):
+    {
+        "shell": true,
+        "cmd": ["iverilog -o ${file_path}/${file_base_name} ${file} && vvp ${file_path}/${file_base_name}"],
+        "file_regex": "^(..[^:]*):([0-9]+):?([0-9]+)?:? (.*)$",
+        "working_dir": "${file_path}",
+    }
+*/
