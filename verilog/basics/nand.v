7bedc8b1be5c16e1e339cbb92ddfaf6d7b2de908
Michael Sjöberg <random@michaelsjoberg.com>
2021-06-12 23:07:27 +0200

    Update nand.v

--- verilog/basics/nand.v
+++ verilog/basics/nand.v
@@ -1,48 +1,49 @@
 // gate-level
-module nand_gate (output Y, input A, B);
-    // wire is electrical connection
-    wire Yd;
-    
-    // operations (gate-level)
-    and(Yd, A, B);                  // Yd = 1 if A = 1 and B = 1, else Yd = 0
-    not(Y, Yd);                     // Y = 0 if Yd = 1, else Y = 1
+module nand_gate (output out, input a, b);
+    // wire : internal pin
+    wire w;
+    // operations
+    and(w, a, b);
+    not(out, w);
 endmodule
 
 // behavioural-level
-module nand_behavioural (output reg Y, input A, B);
-    always @ (A or B) begin
-        if (A == 1'b1 & B == 1'b1) begin
-            Y = 1'b0;
+module nand_behavioural (output reg out, input a, b);
+    always @ (a or b) begin
+        // note: 1'b : 1 bit
+        if (a == 1'b1 & b == 1'b1) begin
+            out = 1'b0;
         end
         else begin
-            Y = 1'b1;
+            out = 1'b1;
         end
     end
 endmodule
 
 // test
-module nand_test;
-    reg A, B;
-    wire Y;
+module test;
+    reg a, b;
+    wire out;
     
-    // nand_gate Instance0 (Y, A, B);
-    nand_behavioural Instance0 (Y, A, B);
+    // nand_gate Instance0 (out, a, b);
+    nand_behavioural Instance0 (out, a, b);
 
+    // note: #(n) : delay by n timestep (check time scale with $printtimescale)
     initial begin
-        #(0) A = 0; B = 0;
-        #(1) A = 0; B = 1;          // delay by one timestep
-        #(1) A = 1; B = 0;          // delay by one timestep
-        #(1) A = 1; B = 1;          // delay by one timestep
+        #(0) a = 0; b = 0;
+        #(1) a = 0; b = 1;
+        #(1) a = 1; b = 0;
+        #(1) a = 1; b = 1;
     end
 
     initial begin
         $printtimescale;
         // Time scale of (_nand_test) is 1s / 1s
         
-        $monitor ("%t | A = %d| B = %d| Y = %d", $time, A, B, Y);
-        // 0 | A = 0| B = 0| Y = 1
-        // 1 | A = 0| B = 1| Y = 1
-        // 2 | A = 1| B = 0| Y = 1
-        // 3 | A = 1| B = 1| Y = 0
+        $monitor ("%t | a = %d | b = %d | out = %d", $time, a, b, out);
+        // 0 | a = 0 | b = 0 | out = 1
+        // 1 | a = 0 | b = 1 | out = 1
+        // 2 | a = 1 | b = 0 | out = 1
+        // 3 | a = 1 | b = 1 | out = 0
     end
 endmodule
