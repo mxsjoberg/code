9e339b75df77f24ae1cdde1eb20edf07f7681178
Michael Sjöberg <random@michaelsjoberg.com>
2021-06-13 01:38:08 +0200

    Update nand.v

--- verilog/basics/nand.v
+++ verilog/basics/nand.v
@@ -1,3 +1,7 @@
+/*
+    Nand(a, b) : If a = b = 1 then out = 0 else out = 1
+*/
+
 // gate-level
 module nand_gate (output out, input a, b);
     // wire : internal pin
@@ -30,17 +34,16 @@ module test;
 
     // note: #(n) : delay by n timestep (check time scale with $printtimescale)
     initial begin
-        #(0) a = 0; b = 0;
-        #(1) a = 0; b = 1;
-        #(1) a = 1; b = 0;
-        #(1) a = 1; b = 1;
+        #(0) a = 0; b = 0; // 1
+        #(1) a = 0; b = 1; // 1
+        #(1) a = 1; b = 0; // 1
+        #(1) a = 1; b = 1; // 0
     end
 
     initial begin
         $printtimescale;
         // Time scale of (_nand_test) is 1s / 1s
-        
-        $monitor ("%t | a = %d | b = %d | out = %d", $time, a, b, out);
+        $monitor ("%1d | a = %d | b = %d | out = %d", $time, a, b, out);
         // 0 | a = 0 | b = 0 | out = 1
         // 1 | a = 0 | b = 1 | out = 1
         // 2 | a = 1 | b = 0 | out = 1
