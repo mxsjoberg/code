09a3f62aa281493178dde796900be5aa39a71223
Michael Sjöberg <random@michaelsjoberg.com>
2021-06-14 21:17:21 +0200

    Update mux16.v

--- verilog/basics/mux16.v
+++ verilog/basics/mux16.v
@@ -2,65 +2,42 @@
     Mux16(a, b, sel) : If sel = 0 then for i = 0..15 out[i] = a[i] else for i = 0..15 out[i] = b[i]
 */
 
+// mux.v
+module mux_gate (output out, input a, b, sel);
+    // wire : internal pin
+    wire w1;
+    wire w2;
+    wire notsel;
+    // operations
+    not(notsel, sel);
+    and(w1, a, notsel);
+    and(w2, b, sel);
+    or(out, w1, w2);
+endmodule
+
 // gate-level
 module mux16_gate (output [15:0] out, input [15:0] a, b, input sel);
     // wire : internal pin
     wire [15:0] w1;
     wire [15:0] w2;
     wire notsel;
-    // operations : not
-    not(notsel, sel);
-    // operations : and : a
-    and(w1[0], a[0], notsel);
-    and(w1[1], a[1], notsel);
-    and(w1[2], a[2], notsel);
-    and(w1[3], a[3], notsel);
-    and(w1[4], a[4], notsel);
-    and(w1[5], a[5], notsel);
-    and(w1[6], a[6], notsel);
-    and(w1[7], a[7], notsel);
-    and(w1[8], a[8], notsel);
-    and(w1[9], a[9], notsel);
-    and(w1[10], a[10], notsel);
-    and(w1[11], a[11], notsel);
-    and(w1[12], a[12], notsel);
-    and(w1[13], a[13], notsel);
-    and(w1[14], a[14], notsel);
-    and(w1[15], a[15], notsel);
-    // operations : and : b
-    and(w2[0], b[0], sel);
-    and(w2[1], b[1], sel);
-    and(w2[2], b[2], sel);
-    and(w2[3], b[3], sel);
-    and(w2[4], b[4], sel);
-    and(w2[5], b[5], sel);
-    and(w2[6], b[6], sel);
-    and(w2[7], b[7], sel);
-    and(w2[8], b[8], sel);
-    and(w2[9], b[9], sel);
-    and(w2[10], b[10], sel);
-    and(w2[11], b[11], sel);
-    and(w2[12], b[12], sel);
-    and(w2[13], b[13], sel);
-    and(w2[14], b[14], sel);
-    and(w2[15], b[15], sel);
-    // operations : or
-    or(out[0], w1[0], w2[0]);
-    or(out[1], w1[1], w2[1]);
-    or(out[2], w1[2], w2[2]);
-    or(out[3], w1[3], w2[3]);
-    or(out[4], w1[4], w2[4]);
-    or(out[5], w1[5], w2[5]);
-    or(out[6], w1[6], w2[6]);
-    or(out[7], w1[7], w2[7]);
-    or(out[8], w1[8], w2[8]);
-    or(out[9], w1[9], w2[9]);
-    or(out[10], w1[10], w2[10]);
-    or(out[11], w1[11], w2[11]);
-    or(out[12], w1[12], w2[12]);
-    or(out[13], w1[13], w2[13]);
-    or(out[14], w1[14], w2[14]);
-    or(out[15], w1[15], w2[15]);
+
+    mux_gate Instance0 (out[0], a[0], b[0], sel);
+    mux_gate Instance1 (out[1], a[1], b[1], sel);
+    mux_gate Instance2 (out[2], a[2], b[2], sel);
+    mux_gate Instance3 (out[3], a[3], b[3], sel);
+    mux_gate Instance4 (out[4], a[4], b[4], sel);
+    mux_gate Instance5 (out[5], a[5], b[5], sel);
+    mux_gate Instance6 (out[6], a[6], b[6], sel);
+    mux_gate Instance7 (out[7], a[7], b[7], sel);
+    mux_gate Instance8 (out[8], a[8], b[8], sel);
+    mux_gate Instance9 (out[9], a[9], b[9], sel);
+    mux_gate Instance10 (out[10], a[10], b[10], sel);
+    mux_gate Instance11 (out[11], a[11], b[11], sel);
+    mux_gate Instance12 (out[12], a[12], b[12], sel);
+    mux_gate Instance13 (out[13], a[13], b[13], sel);
+    mux_gate Instance14 (out[14], a[14], b[14], sel);
+    mux_gate Instance15 (out[15], a[15], b[15], sel);
 endmodule
 
 // behavioural-level
