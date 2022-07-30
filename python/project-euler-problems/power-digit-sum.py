d72d167add7fd85e3bde11a28338c4718e48dc43
Michael Sjöberg <random@michaelsjoberg.com>
2022-07-30 22:37:03 +0200

    Update power-digit-sum.py

--- python/project-euler-problems/power-digit-sum.py
+++ python/project-euler-problems/power-digit-sum.py
@@ -3,6 +3,8 @@
 # 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 #
 # What is the sum of the digits of the number 2^1000?
+#
+# https://projecteuler.net/problem=16
 
 def pow_sum(n, p):
     return sum(list(map(int, list(str(n ** p)))))
