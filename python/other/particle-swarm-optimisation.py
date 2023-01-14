366810835cf8da55c68cf4d58e603389dab0b02e
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-01-14 19:20:58 +0100

    Update particle-swarm-optimisation.py

--- python/other/particle-swarm-optimisation.py
+++ python/other/particle-swarm-optimisation.py
@@ -1,4 +1,3 @@
-# https://en.wikipedia.org/wiki/Particle_swarm_optimization
 import random
 import math
 
@@ -13,12 +12,11 @@ def create_swarm(x_0, n_par):
     dimensions = len(x_0)
     swarm = []
     # generate particles
-    # particle -> (dimensions, position, position_best, velocity, error, error_best)
     for i in range(0, n_par):        
-        # particle position
+        # particle positions
         position = []
         # best position
-        position_best = position
+        position_best = -1
         # particle velocity
         velocity = []
         # particle error
@@ -29,7 +27,7 @@ def create_swarm(x_0, n_par):
             velocity.append(random.uniform(-1, 1))
             position.append(x_0[i])
         # append particle
-        swarm.append((dimensions, position, position_best, velocity, error, error_best))
+        swarm.append([dimensions, position, position_best, velocity, error, error_best])
 
     return swarm
 
@@ -58,10 +56,12 @@ def optimize(f, swarm, bounds, max_iterations, global_best=-1, global_pos=-1):
             velocity = swarm[j][3]
             error = f(position)
             error_best = swarm[j][5]
+            # update error
+            swarm[j][4] = error
             # update individual best if current position is best
             if (error < error_best or error_best == -1):
-                position_best = position
-                error_best = error
+                swarm[j][2] = position
+                swarm[j][5] = error
             # update global best if current particle is best
             if (error < global_best or global_best == -1):
                 global_pos = list(position)
@@ -88,7 +88,8 @@ def optimize(f, swarm, bounds, max_iterations, global_best=-1, global_pos=-1):
 # minimize x^5 - 3x^4 + 5 over [0, 4]
 # x: [x_1, x_2, ..., x_n]
 def f_1(x): return x[0] ** 5 - 3 * x[0] ** 4 + 5
-result = optimize(f_1, create_swarm(x_0=[5], n_par=15), bounds=[(0, 4)], max_iterations=50)
+swarm = create_swarm(x_0=[5], n_par=15)
+result = optimize(f_1, swarm, bounds=[(0, 4)], max_iterations=50)
 # print(result)
 # (-14.91, [2.4])
 
@@ -97,7 +98,8 @@ assert optimize(f_1, create_swarm(x_0=[5], n_par=15), bounds=[(0, 4)], max_itera
 # minimize -(5 + 3x - 4y - x^2 + x y - y^2)
 # x: [x_1, x_2, ..., x_n]
 def f_2(x): return -(5 + 3 * x[0] - 4 * x[1] - x[0] ** 2 + x[0] * x[1] - x[1] ** 2)
-result = optimize(f_2, create_swarm(x_0=[5, 5], n_par=15), bounds=None, max_iterations=50)
+swarm = create_swarm(x_0=[5, 5], n_par=15)
+result = optimize(f_2, swarm, bounds=None, max_iterations=50)
 # print(result)
 # (-9.33, [0.67, -1.67])
 
