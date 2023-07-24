2751f862bd5c95d0680954c1dc53b275fc7fa92d
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-07-24 22:58:12 +0200

    Create enumerate.py

---
+++ python/misc/enumerate.py
@@ -0,0 +1,8 @@
+my_list = ["Zero", "One", "Two", "Three", "Four", "Five"]
+
+for item in enumerate(my_list): print(item)
+# (0, 'First')
+# (1, 'Second')
+# (2, 'Third')
+# (3, 'Fourth')
+# (4, 'Fifth')
