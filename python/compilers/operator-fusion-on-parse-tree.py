0ac9f4e37ae9a06029b2d2dc7a29b882dc672085
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-07-26 01:04:49 +0200

    Create operator-fusion-on-parse-tree.py

---
+++ python/compilers/operator-fusion-on-parse-tree.py
@@ -0,0 +1,26 @@
+from dis import dis
+
+def simplify_parse_tree(tree):
+    if (isinstance(tree, tuple)):
+        # first value in tuple, remaining values in tuple
+        operator, *operands = tree
+        operands = [simplify_parse_tree(operand) for operand in operands]
+
+        if all(isinstance(operand, int) for operand in operands):
+            if operator == '+':
+                return sum(operands)
+            elif operator == '*':
+                return eval('*'.join(map(str, operands)))
+
+    return tree
+
+# (1 + 2) * (3 + 4)
+parse_tree = ('*', ('+', 1, 2), ('+', 3, 4))
+print(simplify_parse_tree(parse_tree))
+# 21
+
+compiled = compile('(1 + 2) * (3 + 4)', '<string>', 'eval')
+print(compiled.co_consts[0])
+# 21
+
+assert simplify_parse_tree(parse_tree) == compiled.co_consts[0]
