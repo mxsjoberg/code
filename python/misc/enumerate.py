215aeba70995bbe45810800800b9248c748fde53
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-28 09:55:00 +0200

    Revert "Delete python directory"
    This reverts commit f07c1cc65dc522ca4eeceef0b084aaf4a625464d.

---
+++ python/misc/enumerate.py
@@ -0,0 +1,10 @@
+# 2018-06
+
+my_list = ["Zero", "One", "Two", "Three", "Four", "Five"]
+
+for item in enumerate(my_list): print(item)
+# (0, 'First')
+# (1, 'Second')
+# (2, 'Third')
+# (3, 'Fourth')
+# (4, 'Fifth')
