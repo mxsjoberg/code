cf3264198862bddcbe2c59b7582fea4b2019544e
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-11-21 15:58:30 +0800

    python ml

---
+++ python/ml/backpropagation.py
@@ -0,0 +1,51 @@
+# https://en.wikipedia.org/wiki/Backpropagation
+import numpy as np
+
+np.random.seed(42)
+
+# https://en.wikipedia.org/wiki/Sigmoid_function
+def sigmoid(x): return 1.0 / (1 + np.exp(-x))
+assert(sigmoid(0) == 0.5)
+
+# derivative of sigmoid
+def sigmoid_dx(x): return x * (1.0 - x)
+assert(sigmoid_dx(0.5) == 0.25)
+
+# input
+x = np.array([[0,0,1], [0,1,1], [1,0,1], [1,1,1]])
+y = np.array([[0], [1], [1], [0]])
+# weights
+w_1 = np.random.rand(x.shape[1],4)
+w_2 = np.random.rand(4,1)
+# output
+output = np.zeros(y.shape)
+
+def feedforward(x, w_1, w_2, output):
+    layer_1 = sigmoid(np.dot(x, w_1))
+    output = sigmoid(np.dot(layer_1, w_2))
+    return layer_1, w_1, w_2, output
+
+def backpropagation(layer_1, w_1, w_2, x, y, output):
+    w_2_dx = np.dot(
+        np.transpose(layer_1),
+        (2 * (y - output) * sigmoid_dx(output))
+    )
+    w_1_dx = np.dot(
+        np.transpose(x),
+        (np.dot(2 * (y - output) * sigmoid_dx(output), np.transpose(w_2)) * sigmoid_dx(layer_1))
+    )
+    # update weights
+    w_1 += w_1_dx
+    w_2 += w_2_dx
+
+    return layer_1, w_1, w_2, x, y, output
+
+for i in range(1500):
+    layer_1, w_1, w_2, output = feedforward(x, w_1, w_2, output)
+    layer_1, w_1, w_2, x, y, output = backpropagation(layer_1, w_1, w_2, x, y, output)
+
+print(output)
+# [[0.01043733]
+#  [0.97230567]
+#  [0.97090151]
+#  [0.03501066]]
---
+++ python/ml/numpy-linear-relu.py
@@ -0,0 +1,34 @@
+"""
+A simple implementation of linear layer followed by a
+ReLU activation function.
+"""
+
+import numpy as np
+
+dtype = "float32"
+
+# numpy
+a_np = np.random.rand(128, 128).astype(dtype)
+b_np = np.random.rand(128, 128).astype(dtype)
+# matmul -> relu
+c_mm_relu = np.maximum(a_np @ b_np, 0)
+
+# implementation
+def mm_relu(A: np.ndarray, B: np.ndarray, C: np.ndarray):
+    # A, B, C: 128x128
+    Y = np.empty((128, 128), dtype=dtype)
+    # matmul
+    for i in range(128):
+        for j in range(128):
+            for k in range(128):
+                if k == 0: Y[i, j] = 0
+                Y[i, j] += A[i, k] * B[k, j]
+    # relu
+    for i in range(128):
+        for j in range(128): C[i, j] = max(Y[i, j], 0)
+
+# test
+c_np = np.empty((128, 128), dtype=dtype)
+mm_relu(a_np, b_np, c_np)
+# check within tolerance
+np.testing.assert_allclose(c_mm_relu, c_np, rtol=1e-5)
