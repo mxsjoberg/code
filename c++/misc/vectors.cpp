58801fc5c09e5da7c0104c0b3186a42a51d7a17a
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-11-24 10:35:59 +0800

    Update vectors.cpp

--- c++/misc/vectors.cpp
+++ c++/misc/vectors.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <format>
 
 int main() {
     // vector of ints
-    std::vector<int> vec;
+    std::vector<int> numbers;
     // size
-    std::cout << vec.size() << std::endl; // 0
-
+    std::cout << numbers.size() << std::endl; // 0
     // push values
     for (int i = 1; i <= 5; i++) {
-        vec.push_back(i);
+        numbers.push_back(i);
     }
-    std::cout << vec.size() << std::endl; // 5
+    std::cout << numbers.size() << std::endl; // 5
+    std::cout << numbers[3] << std::endl; // 4
 
-    // access values
-    std::cout << vec[3] << std::endl; // 4
+    // vector of strings
+    std::vector<std::string> strings = { "hello", "c++" };
+    std::cout << strings[0] << " " << strings[1] << std::endl; // hello c++
 }
